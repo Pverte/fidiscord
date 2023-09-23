f8f4b1f9ec4fe5b9dcbed84afa56394999f127cc
Pverte <63103558+Pverte@users.noreply.github.com>
2023-09-23 22:51:26 +0200

    trying

--- main.py
+++ main.py
@@ -174,6 +174,7 @@ async def sync_level_roles(ctx):
             roles_to_add.append(role_id)
 
     # Add the roles to the user
+    print("before adding the role")
     await ctx.author.add_roles(*roles_to_add)
     print("Added the roles to the user")
 
